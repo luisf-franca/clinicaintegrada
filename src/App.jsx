bfcc5f1b0713eac5baaf0b54c1e1a1863a6ae62e
Luís Filipe França <112729610+luisf-franca@users.noreply.github.com>
2024-11-05 09:43:29 -0300

    criação do ambiente de desenvolvimento

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// COMPONENTS
+import Router from './components/Router/Router.jsx';
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
