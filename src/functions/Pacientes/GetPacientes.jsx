2ac1b4b0c8c5e20d4b9742a595f56fc9008bef95
Caioliu <caioarthur_@hotmail.com>
2024-11-24 23:09:28 -0300

    implementando GetAll pacientes

---
+++ src/functions/Pacientes/GetPacientes.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const GetPacientes = async (options = {}) => {
+  try {
+    // Extrai os parâmetros opcionais
+    const { page, pageSize, filter, orderBy } = options;
+    console.log('options:', options);
+
+    // Monta a query string com os parâmetros opcionais
+    const params = new URLSearchParams();
+    if (page) params.append('page', page);
+    if (pageSize) params.append('pageSize', pageSize);
+    if (filter) params.append('filter', filter);
+    if (orderBy) params.append('orderBy', orderBy);
+
+    // Monta a URL completa com a query string
+    const url = import.meta.env.VITE_API_BASE_URL;
+    const fullUrl = `${url}/pacientes?${params.toString()}`;
+
+    // Obtém o token de autenticação
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImF0ZW5kZW50ZUB1c2VyLmNvbS5iciIsImp0aSI6ImIyOGU5MmJkLThhYzUtNDU3NS04Mjg2LTNmZTg1MmFiMGU5MiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJhdGVuZGVudGVAdXNlci5jb20uYnIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJhdGVuZGVudGUiLCJleHAiOjE3MzI1Mjg3NzcsImlzcyI6IkNsaW5pY2FJbnRlZ3JhZGFfSXNzdWVyIiwiYXVkIjoiQ2xpbmljYUludGVncmFkYV9BdWRpZW5jZSJ9.FedLtD4dxRngTj2okFj3jX5mVTFxXltRCmMNKDEVOuI';
+
+    // Faz a requisição com os parâmetros e o cabeçalho de autenticação
+    const response = await axios.get(fullUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Inclui o token no cabeçalho
+      },
+    });
+
+    console.log('response:', response);
+    return response.data.data.items;
+    // return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar pacientes:', error);
+    throw error;
+  }
+};
+
+export default GetPacientes;
